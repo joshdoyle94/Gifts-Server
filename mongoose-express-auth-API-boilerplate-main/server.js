55238f08d0b32b92b2f9b88d219ed6d8282f5c47
joshdoyle94 <joshdoyle94@gmail.com>
2022-11-07 13:26:24 -0500

    added first commit

--- mongoose-express-auth-API-boilerplate-main/server.js
+++ mongoose-express-auth-API-boilerplate-main/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
+const giftRoutes = require('./app/routes/gift_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -65,7 +65,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+app.use(giftRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
@@ -75,7 +75,7 @@ app.use(errorHandler)
 
 // run API on designated port (4741 in this case)
 app.listen(port, () => {
-	console.log('listening on port ' + port)
+	console.log('Now listening to the sweet sounds of port:' + port)
 })
 
 // needed for testing
